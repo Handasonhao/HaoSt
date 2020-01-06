6d012896d9986fcd1e3a89b891a442375ece73c4
Handasonhao <1546356515@qq.com>
2020-01-06 14:16:48 +0800

    新增页面

---
+++ HaHa/pages/tabbar/components/whh-crop-picture-upload/cropPictureUpload.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="content">
+		<whh-crop-picture-upload></whh-crop-picture-upload>
+	</view>
+</template>
+
+<script>
+	import whhCropPictureUpload from '../../../../components/whh-crop-picture-upload/whhCropPictureUpload.vue'
+	export default {
+		data(){
+			return {
+				
+			}
+		},
+		components:{
+			whhCropPictureUpload
+		}
+	}
+</script>
+
+<style>
+</style>
